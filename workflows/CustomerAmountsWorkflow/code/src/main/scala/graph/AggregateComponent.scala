9953bdd5028774f72212ab9bcff5aec4cfbb771c
eng <eng@prophecy.io>
2021-02-03 09:44:57 +0000

    New Commit for Workflow Code

--- workflows/CustomerAmountsWorkflow/code/src/main/scala/graph/AggregateComponent.scala
+++ workflows/CustomerAmountsWorkflow/code/src/main/scala/graph/AggregateComponent.scala
@@ -14,7 +14,7 @@ import org.apache.spark.sql.functions._
 import config.ConfigStore._
 import graph._
 
-@Visual(id = "AggregateComponent", label = "AggregateComponent", x = 373, y = 98, phase = 0)
+@Visual(id = "AggregateComponent", label = "AggregateComponent", x = 373, y = 98, phase = 0, detailedStats = true)
 object AggregateComponent {
 
   def apply(spark: SparkSession, in: DataFrame): Aggregate = {
