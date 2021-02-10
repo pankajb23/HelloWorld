566c829f1b72ff5aa56324833b77031933b05c7c
eng <eng@prophecy.io>
2021-02-10 08:57:50 +0000

    New Commit for Workflow Code

--- workflows/PrepareForBankWorkflow/code/src/main/scala/main.scala
+++ workflows/PrepareForBankWorkflow/code/src/main/scala/main.scala
@@ -18,11 +18,11 @@ object Main {
 
   def graph(spark: SparkSession): Unit = {
 
-    FinalDatasetOutput(spark)
     val df_CustomerOrdersDatasetInput: Source      = CustomerOrdersDatasetInput(spark)
     val df_PrepareComponent:           Reformat    = PrepareComponent(spark, df_CustomerOrdersDatasetInput)
     val df_Repartition0:               Repartition = Repartition0(spark,     df_PrepareComponent)
-    val df_AggReportComponent:         Aggregate   = AggReportComponent(spark)
+    FinalDatasetOutput(spark, df_Repartition0)
+    val df_AggReportComponent: Aggregate = AggReportComponent(spark, df_PrepareComponent)
     ReportDatasetOutput(spark, df_AggReportComponent)
 
   }
