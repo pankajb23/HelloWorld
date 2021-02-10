86bd92a9133e589765ac3d899140f354bef5b26e
eng <eng@prophecy.io>
2021-02-10 17:27:55 +0000

    New Commit for Workflow Code

---
+++ workflows/CustomerAmountsWorkflow/code/src/main/scala/graph/Repartition0.scala
@@ -0,0 +1,29 @@
+package graph
+
+import org.apache.spark.sql.types._
+import io.prophecy.libs._
+import io.prophecy.libs.UDFUtils._
+import io.prophecy.libs.Component._
+import io.prophecy.libs.DataHelpers._
+import io.prophecy.libs.SparkFunctions._
+import io.prophecy.libs.FixedFileFormatImplicits._
+import org.apache.spark.sql.ProphecyDataFrame._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import config.ConfigStore._
+import graph._
+
+@Visual(id = "Repartition0", label = "Repartition0", x = 718, y = 97, phase = 0)
+object Repartition0 {
+
+  def apply(spark: SparkSession, in: DataFrame): Repartition = {
+    import spark.implicits._
+
+    val out = in.coalesce(10)
+
+    out
+
+  }
+
+}
