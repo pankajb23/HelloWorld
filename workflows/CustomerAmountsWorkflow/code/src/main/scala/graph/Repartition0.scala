e34b717da6a191f76edf916cd9f454bb3f2d38a0
eng <eng@prophecy.io>
2021-02-17 12:11:23 +0000

    New Commit for Workflow Code

--- workflows/CustomerAmountsWorkflow/code/src/main/scala/graph/Repartition0.scala
+++ workflows/CustomerAmountsWorkflow/code/src/main/scala/graph/Repartition0.scala
@@ -12,6 +12,7 @@ import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import config.ConfigStore._
+import udfs.UDFs._
 import graph._
 
 @Visual(id = "Repartition0", label = "Repartition0", x = 1003, y = 196, phase = 0)
@@ -20,7 +21,7 @@ object Repartition0 {
   def apply(spark: SparkSession, in: DataFrame): Repartition = {
     import spark.implicits._
 
-    val out = in.repartition(col("country_code"))
+    val out = in.coalesce(1)
 
     out
 
