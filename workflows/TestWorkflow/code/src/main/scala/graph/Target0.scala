54199f8462afd3e487049fe5564a38c1868bce93
eng <eng@prophecy.io>
2021-03-26 16:49:30 +0000

    New Commit for Workflow Code

---
+++ workflows/TestWorkflow/code/src/main/scala/graph/Target0.scala
@@ -0,0 +1,51 @@
+package graph
+
+import io.prophecy.libs._
+import io.prophecy.libs.UDFUtils._
+import io.prophecy.libs.Component._
+import io.prophecy.libs.DataHelpers._
+import io.prophecy.libs.SparkFunctions._
+import io.prophecy.libs.FixedFileFormatImplicits._
+import org.apache.spark.sql.ProphecyDataFrame._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import config.ConfigStore._
+import udfs.UDFs._
+import graph._
+
+@Visual(id = "Target0", label = "Target0", x = 442, y = 47, phase = 0)
+object Target0 {
+
+  @UsesDataset(id = "15", version = 0)
+  def apply(spark: SparkSession, in: DataFrame): Target = {
+    import spark.implicits._
+
+    val fabric = Config.fabricName
+    fabric match {
+      case "azdb" =>
+        val schemaArg = StructType(
+          Array(
+            StructField("customer_id",       IntegerType, false),
+            StructField("first_name",        StringType,  false),
+            StructField("last_name",         StringType,  false),
+            StructField("phone",             StringType,  false),
+            StructField("email",             StringType,  false),
+            StructField("country_code",      StringType,  false),
+            StructField("account_open_date", StringType,  false),
+            StructField("account_flags",     StringType,  false)
+          )
+        )
+        in.write
+          .format("csv")
+          .option("header", true)
+          .option("sep",    ",")
+          .mode("append")
+          .save("dbfs:/Users/prophecy/eng/CustomersDatasetInput.csv")
+      case _ => throw new Exception("Unknown Fabric")
+    }
+
+  }
+
+}
