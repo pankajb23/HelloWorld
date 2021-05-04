68de69d49ea06004cf132bb97098e64597bc740e
prophecy-system <system@prophecy.io>
2021-05-04 11:47:29 +0000

    Title (#67)

--- schedules/rajat3/code/main.py
+++ schedules/rajat3/code/main.py
@@ -4,7 +4,8 @@ from airflow import DAG
 config = load_config()
 with DAG(**config.dag_args()) as the_dag:
     _Workflow_0 = Workflow_0(config)
+    _Workflow_1 = Workflow_1(config)
 
-    
+    _Workflow_0[1] >> _Workflow_1[0]
 
 
