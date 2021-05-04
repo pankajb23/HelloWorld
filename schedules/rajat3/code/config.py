dd4c58ffba3545918bd4c4b8d03807742e899eec
prophecy-system <system@prophecy.io>
2021-05-04 09:56:31 +0000

    Title (#58)

---
+++ schedules/rajat3/code/config.py
@@ -0,0 +1,20 @@
+
+
+
+class Config(object):
+
+    def __init__(self, fabric: str="common"):
+        self.fabric = fabric
+        self.dag_id = "rajat3333"
+        self.timezone = "US/Pacific"
+        self.start_date = "2021-05-04T09:54:38"
+        self.schedule_interval = "@daily"
+        self.cluster_id = "eng.pankaj.002@prophecy.io_49_connId"
+
+    def dag_args(self):
+        return {
+            'dag_id': self.dag_id,
+            'default_args': {
+"depends_on_past" : False, "start_date" : self.start_date},
+            "schedule_interval": self.schedule_interval
+        }
