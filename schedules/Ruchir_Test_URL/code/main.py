3311c5ef1e3220085c89a58678ac69aa19ef61bb
prophecy-system <system@prophecy.io>
2021-05-05 09:57:37 +0000

    Title (#103)

--- schedules/Ruchir_Test_URL/code/main.py
+++ schedules/Ruchir_Test_URL/code/main.py
@@ -3,8 +3,9 @@ from airflow import DAG
 
 config = load_config()
 with DAG(**config.dag_args()) as the_dag:
+    op_workflow_0 = Workflow_0(config)
     op_script_0 = Script_0(config)
 
-    
+    op_workflow_0[1] >> op_script_0
 
 
