8a4a833e9a9adea527eb21c446dd78536531a630
prophecy-system <system@prophecy.io>
2021-05-06 22:29:25 +0000

    Title (#115)

--- schedules/rajat2/code/main.py
+++ schedules/rajat2/code/main.py
@@ -3,7 +3,7 @@ from airflow import DAG
 
 config = load_config()
 with DAG(**config.dag_args()) as the_dag:
-    _Test_Script = Test_Script(config)
+    op_test_script = Test_Script(config)
 
     
 
