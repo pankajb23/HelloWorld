2e0a92c458599e9c2f5a7a2c842117feef50032e
eng <eng@prophecy.io>
2021-05-26 07:25:41 +0000

    New Commit for Schedule Code

---
+++ schedules/Ruchir_Test_URL/code/graph/Sensor_0.py
@@ -0,0 +1,18 @@
+from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
+from airflow.models import BaseOperator
+
+def Sensor_0(config):
+    sensor = S3KeySensor(
+        bucket_key = "",
+        wildcard_match = False,
+        aws_conn_id = "",
+        verify = False,
+        soft_fail = False,
+        poke_interval = 60,
+        timeout = 604800,
+        mode = "poke",
+        task_id = "Sensor_0",
+        exponential_backoff = False
+    )
+
+    return sensor
